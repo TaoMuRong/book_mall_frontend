898118c1dfd9abf36ede73b2f4e1d7883cd7e714
tianzexiang <845013957@qq.com>
2021-03-23 22:01:37 +0800

    login&primary color

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/theme/index.css';
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <el-form :inline="true" :model="userInfo" class="demo-form-inline">
-      <el-form-item label="用户名">
-        <el-input v-model="userInfo.userName" placeholder="用户名"></el-input>
-      </el-form-item>
-      <el-form-item label="密码">
-        <el-input
+  <div id="login-container">
+    <div class="login-form">
+      <div class="login-title">Login</div>
+      <div class="username">
+        <label for="username-input" class="username-label">用户名</label>
+        <input
+          id="username-input"
+          v-model="userInfo.userName"
+          placeholder="用户名"
+        />
+      </div>
+
+      <div class="pwd">
+        <label for="pwd-input" class="pwd-label">密码</label>
+        <input
+          id="pwd-input"
           v-model="userInfo.pwd"
           placeholder="密码"
           type="password"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
-      </el-form-item>
-    </el-form>
+        />
+      </div>
+
+      <el-button type="primary" @click="onSubmit">登录</el-button>
+    </div>
   </div>
 </template>
 
@@ -39,13 +47,13 @@ export default {
       if (data.status) {
         const role = data.result[0];
         this.setRole(role);
-        if(role === "admin") {
-          this.$router.push({path: '/admin/book_management'})
+        if (role === "admin") {
+          this.$router.push({ path: "/admin/book_management" });
         } else {
-          this.$router.push({path: '/home/book_mall'})
+          this.$router.push({ path: "/home/book_mall" });
         }
       } else {
-        window.alert("login fail!")
+        window.alert("login fail!");
       }
     },
   },
@@ -53,4 +61,82 @@ export default {
 </script>
 
 <style scoped lang="less">
+@login-input-wrap-width: 80%;
+
+@label-height: 40px;
+@label-width: 80px;
+
+@input-height: 30px;
+@input-width: 200px;
+
+.input-style(@input-width,@label-height) {
+  &:focus {
+    outline: none;
+  }
+  width: @input-width;
+  height: @label-height;
+  border-radius: 999px;
+  padding-left: 10px;
+  border: none;
+}
+
+.label-style(@label-height,@label-width) {
+  text-align: center;
+  height: @label-height;
+  line-height: @label-height;
+  width: @label-width;
+}
+.input-wrap-style(@login-input-wrap-width) {
+  display: flex;
+  align-items: center;
+  width: @login-input-wrap-width;
+}
+
+.el-input {
+  border-radius: 99px;
+  overflow: hidden;
+}
+
+#login-container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../assets/image/beijing.webp") no-repeat;
+  background-size: cover;
+  .login-title{
+    font-size: 22px;
+  }
+  .login-form {
+    width: 400px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.3);
+    box-shadow: 3px 3px 6px 3px rgba(0, 0, 0, 0.3);
+    border-radius: 5px;
+    overflow: hidden;
+    position: relative;
+    .username {
+      .input-wrap-style(@login-input-wrap-width);
+      .username-label {
+        .label-style(@label-height,@label-width);
+      }
+      #username-input {
+        .input-style(@input-width,@label-height);
+      }
+    }
+    .pwd {
+      .input-wrap-style(@login-input-wrap-width);
+      .pwd-label {
+        .label-style(@label-height,@label-width);
+      }
+      #pwd-input {
+        .input-style(@input-width,@label-height);
+      }
+    }
+  }
+}
 </style>
