38609d552aad43be1ce79081a7eb60ba073fa311
Leslieliu <1439433131@qq.com>
2021-05-28 21:39:11 +0800

    拾取任务完成

--- src/views/admin/PickUpTask.vue
+++ src/views/admin/PickUpTask.vue
@@ -1,105 +1,70 @@
 <template>
   <div>
-<!--    <template>-->
-<!--      <el-button @click="clearFilter">清除所有过滤器</el-button>-->
-<!--      <el-table-->
-<!--          ref="filterTable"-->
-<!--          :data="tableData"-->
-<!--          style="width: 100%"-->
-<!--          height="550"-->
-<!--          :default-sort = "{prop: 'applyTime', order: 'descending'}"-->
-<!--          stripe>-->
-<!--        <el-table-column-->
-<!--            prop="applyTime"-->
-<!--            label="申请日期"-->
-<!--            width="300"-->
-<!--            sortable-->
-<!--            column-key="date"-->
-<!--        >-->
-<!--        </el-table-column>-->
-<!--        <el-table-column-->
-<!--            prop="id"-->
-<!--            label="记录ID"-->
-<!--            width="300">-->
-<!--        </el-table-column>-->
-<!--        <el-table-column-->
-<!--            prop="bookId"-->
-<!--            label="书籍ID"-->
-<!--            width="300">-->
-<!--        </el-table-column>-->
-<!--        <el-table-column-->
-<!--            prop="tag"-->
-<!--            label="状态"-->
-<!--            width="200"-->
-<!--            :filters="[{ text: 'PENDING', value: 'PENDING' }, { text: 'APPLY_FAILED', value: 'APPLY_FAILED' }, { text: 'APPLY_SUCCESS', value: 'APPLY_SUCCESS' }]"-->
-<!--            :filter-method="filterTag"-->
-<!--            filter-placement="bottom-end">-->
-<!--          <template slot-scope="scope">-->
-<!--            <el-tag-->
-<!--                :type="scope.row.status === 'PENDING' ? 'primary' : 'success'"-->
-<!--                disable-transitions>{{scope.row.status}}</el-tag>-->
-<!--          </template>-->
-<!--        </el-table-column>-->
-<!--        <el-table-column-->
-<!--            prop="name"-->
-<!--            label="操作"-->
-<!--            width="180">-->
-<!--          <template slot-scope="scope">-->
-<!--            <el-button type="primary"-->
-<!--                       :disabled="scope.row.status === 'PENDING' ? true : false"-->
-<!--                       @click="openDialog(scope.row.id)"-->
-<!--            >-->
-<!--              查看详情-->
-<!--            </el-button>-->
-
-<!--            &lt;!&ndash;            点击出现的对话框&ndash;&gt;-->
-<!--            <el-dialog title="审核结果" :visible.sync="dialogFormVisible" width="30%">-->
-<!--              <el-form :model="form">-->
-<!--                <el-form-item label="审核时间" :label-width="formLabelWidth" >-->
-<!--                  <el-input v-model="form.time" :disabled="true"></el-input>-->
-<!--                </el-form-item>-->
-<!--                <el-form-item label="审核人" :label-width="formLabelWidth" >-->
-<!--                  <el-input v-model="form.userName" :disabled="true"></el-input>-->
-<!--                </el-form-item>-->
-<!--                <el-form-item label="审核意见" :label-width="formLabelWidth">-->
-<!--                  <el-input v-model="form.content" :disabled="true"></el-input>-->
-<!--                </el-form-item>-->
-<!--              </el-form>-->
-<!--              <div slot="footer" class="dialog-footer">-->
-<!--                <el-dialog-->
-<!--                    width="30%"-->
-<!--                    title="入库单"-->
-<!--                    :visible.sync="innerVisible"-->
-<!--                    append-to-body>-->
-<!--                  <el-form label-width="100px">-->
-<!--                    <el-form-item label="申请人：">-->
-<!--                      <el-input v-model="'管理员'+scope.row.applyMemberName" :disabled="true" ></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item label="书籍ID：">-->
-<!--                      <el-input v-model="scope.row.bookId" :disabled="true"></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item label="记录ID：">-->
-<!--                      <el-input v-model="scope.row.id" :disabled="true"></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item label="新入库数：">-->
-<!--                      <el-input v-model="scope.row.stock" ></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item>-->
-<!--                      <el-button type="success" @click="reApply(scope.row)" class="reApplyBtn">重新申请</el-button>-->
-<!--                    </el-form-item>-->
-<!--                  </el-form>-->
-<!--                </el-dialog>-->
-<!--                <el-button type="success" @click="innerVisible = true" class="reApply">调整申请</el-button>-->
-<!--                <div class="btns">-->
-<!--                  <el-button @click="dialogFormVisible = false">取 消</el-button>-->
-<!--                  <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </el-dialog>-->
-<!--          </template>-->
-<!--        </el-table-column>-->
-<!--      </el-table>-->
-<!--    </template>-->
+    <template>
+      <el-button @click="clearFilter">清除所有过滤器</el-button>
+      <el-table
+          ref="filterTable"
+          :data="tableData"
+          style="width: 100%"
+          height="500"
+          :default-sort = "{prop: 'applyTime', order: 'descending'}"
+          stripe>
+        <el-table-column
+            prop="applyTime"
+            label="申请日期"
+            width="300"
+            sortable
+            column-key="date"
+        >
+        </el-table-column>
+        <el-table-column
+            prop="id"
+            label="记录ID"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="bookId"
+            label="书籍ID"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="stock"
+            label="本数"
+            width="300"
+            sortable>
+        </el-table-column>
+        <el-table-column
+            prop="tag"
+            label="状态"
+            width="200"
+            :filters="[{ text: 'PENDING', value: 'PENDING' }, { text: 'APPLY_FAILED', value: 'APPLY_FAILED' }, { text: 'APPLY_SUCCESS', value: 'APPLY_SUCCESS' }]"
+            :filter-method="filterTag"
+            filter-placement="bottom-end">
+          <template slot-scope="scope">
+            <el-tag
+                :type="scope.row.status === 'PENDING' ? 'primary' : 'success'"
+                disable-transitions>{{scope.row.status}}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="操作"
+            width="180">
+          <template slot-scope="scope">
+            <el-button type="primary" @click="pickUpTasks(scope.row.id)">
+              拾取
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+<!--    放置分页按钮-->
+    <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="50"
+        @current-change="currentChange">
+    </el-pagination>
   </div>
 </template>
 
@@ -117,7 +82,7 @@ export default {
       this.$http
           .get('/wareapply/candidate/tasks', {
             params: {
-              limit: 4,
+              limit: 8,
               memberId: localStorage.accountId,
               page: currPage
             }
@@ -131,6 +96,28 @@ export default {
           .catch(function (error) { // 请求失败处理
             console.log(error)
           });
+    },
+    clearFilter() {
+      this.$refs.filterTable.clearFilter();
+    },
+    currentChange (currentPage) {
+      this.getTasks(currentPage)
+    },
+    pickUpTasks (Id) {
+      this.$http
+          .post('/wareapply/claim?memberId=' + localStorage.accountId + '&wareApplyId=' + Id)
+          .then(response => {
+            if (response.status === 200) {
+              this.$message.success("拾取成功！")
+              this.getTasks(1)
+            }
+          })
+          .catch(function (error) { // 请求失败处理
+            console.log(error)
+          });
+    },
+    filterTag(value, row) {
+      return row.status === value;
     }
   },
   created() {
@@ -140,5 +127,9 @@ export default {
 </script>
 
 <style scoped>
-
+.el-pagination {
+  margin-top: 10px;
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
